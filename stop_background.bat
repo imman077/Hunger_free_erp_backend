c6e7f8521f4d2a7be59cf7f68b5582d0b33637e4
Imman <immanuvel@appikorn.com>
2026-06-14 12:34:45 +0530

    Revert "Initial commit with my project"
    This reverts commit 48ad7d9d2af00c83e6550d96ee6dfad8b59deddd.

--- stop_background.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Stopping Dad Bills server...
-powershell -Command "Get-CimInstance Win32_Process -Filter 'Name = ''python.exe''' | Where-Object CommandLine -like '*server.py*' | Remove-CimInstance" >nul 2>&1
-echo Server stopped successfully.
-pause
